f5e46a1a9e70891d6f24b3be3dccac168575ae5a
Vinicius Wichert <vinicius.rampasso@escola.pr.gov.br>
2026-09-17 11:18:59 +0000

    17/09

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ async function buscarclima(cidade){
 const botaoBuscar = document.getElementById("btnBuscar");
 const campoCidade = document.getElementById("cidade");
 const divResultado = document.getElementById("resultado");
+const divPrevisao = document.getElementById("previsao")
 
 botaoBuscar.addEventListener("click", async function(){
     const cidade = campoCidade.value;
@@ -18,9 +19,45 @@ botaoBuscar.addEventListener("click", async function(){
 
     const dados = await buscarclima(cidade);
 
-    if (dados.cod === "404"){   /* tem igualdedade me js é com 3 === */
+    async function realizarBusca(cidade){
+        const dados = await buscarclima(cidade);
+
+         if (dados.cod === "404"){   /* tem igualdedade me js é com 3 === */
         divResultado.innerHTML = "<p> Cidade não encontrada.</p>";
         return;
+        }
+
+        const iconeUrl = `https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`
+
+        divResultado.innerHTML = `
+        <div class="card-clima">
+            <img src="${iconeUrl}" alt="${dados.weather[0].description}">
+            <h3>${dados.name}</h3>
+            <p>${dados.weather[0].description}</p>
+            <p><strong>${dados.main.temp}ºC</strong></p>
+            <p>Sensação: ${dados.main.feels_like}ºC</p>
+        </div>
+    `;
+        localStorage.setItem("ultimacidade",cidade)
+
+        const previsao = await buscarPrevisao(cidade);
+        montaPrevisao(cidade);
+
+function montaPrevisao(previsao){
+    divPrevisao.innerHTML = "";
+}
+    }
+    if (ITEM.DS_TEXT.INCLUDES("12:00:00")){
+    const data = new Date(item.dt_text);
+    const diaSemana = data.toDateString("pt-BR",{weekday: "shot"});
+    const iconeUrl = `https://openweathermap.org/img/wn/${item.wheather[0].icon}.png`
+     
+    
+    async function buscarPrevisao(cidade){
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${CHAVE_API}&units=metric&lang=pt_br`
+        const resposta = await fetch(url);
+        const dados = await resposta.json()
+        return dados;
     }
 
     divResultado.innerHTML = `
@@ -33,3 +70,4 @@ botaoBuscar.addEventListener("click", async function(){
     `;
 
 });
+
